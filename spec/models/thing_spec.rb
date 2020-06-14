92e2f16251f913747b10271e82235e7e19833d84
Matt Bell <matt@mbell.me>
2020-06-14 12:18:58 +0100

    Remove rails tests

--- spec/models/thing_spec.rb
+++ spec/models/thing_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Thing, type: :model do
   it "must validate name" do
-    expect { Thing.create!  }.to raise_error(ActiveRecord::RecordInvalid)
+    thing = Thing.new(name: "Test")
+    expect(thing).to be_valid
+    expect(Thing.new).to_not be_valid
+  end
+  it "can include an image" do
+    thing = Thing.new(name: "Test", header_image: "foobar")
+    expect(thing).to be_valid
   end
 end
