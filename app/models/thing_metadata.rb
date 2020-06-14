6894f6e87501175bcf457c6a9e094d74539f2432
Matt Bell <matt@mbell.dev>
2020-06-14 17:35:52 +0100

    Show underlying thing in API

--- app/models/thing_metadata.rb
+++ app/models/thing_metadata.rb
@@ -2,4 +2,8 @@ class ThingMetadata < ApplicationRecord
   validates :name, presence: true
   validates :header_image, presence: false
   belongs_to :thing, polymorphic: true
+
+  def as_json(options = nil)
+    super({ include: :thing }.merge(options || {}))
+  end
 end
